c1c9085fc08ef1eb6f03a4dfa9158d93e485da2c
Hillel1408 <etemax@bk.ru>
2023-03-10 13:01:06 +0300

    Окунев Павел

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,6 +1,5 @@
 const swiper = new Swiper('.reviews__swiper', {
     direction: 'horizontal',
-    // Navigation arrows
     navigation: {
         nextEl: '.reviews__swiper-button-next',
         prevEl: '.reviews__swiper-button-prev',
@@ -11,5 +10,22 @@ const swiper = new Swiper('.reviews__swiper', {
         renderBullet: function (index, className) {
             return '<span class="' + className + '">' + (index + 1) + '</span>';
         },
+    },
+});
+const workSwiper = new Swiper('.work__swiper', {
+    direction: 'horizontal',
+    allowTouchMove: false,
+    navigation: {
+        nextEl: '.work__swiper-button-next',
+        prevEl: '.work__swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        // Makes the Scrollbar Draggable
+        draggable: true,
+        // Snaps slider position to slides when you release Scrollbar
+        snapOnRelease: true,
+        // Size (Length) of Scrollbar Draggable Element in px
+        dragSize: 'auto',
     },
 });
